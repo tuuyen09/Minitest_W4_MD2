f4df862cfa8b3fcf1ad41a21ed4888f6a91e4fee
tuuyen09 <tu190901@gmail.com>
2023-04-16 12:32:19 +0700

    WriteFile

--- src/WriteFile.java
+++ src/WriteFile.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class WriteFile {
+import java.io.*;
+import java.util.List;
+
+public class WriteFile {
+    public static void writeFile (List<Contact> contactList){
+        File file = new File("contact.txt");
+        try {
+            OutputStream os = new FileOutputStream(file);
+            ObjectOutputStream oos = new ObjectOutputStream(os);
+            oos.writeObject(contactList);
+            os.close();
+            oos.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
