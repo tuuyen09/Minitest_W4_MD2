4f36598d01dd323e8eef2ef8c31c3fdd4826f371
tuuyen09 <tu190901@gmail.com>
2023-04-16 12:31:21 +0700

    Sort

--- src/SortContactList.java
+++ src/SortContactList.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class SortContactList {
+import java.util.Comparator;
+
+public class SortContactList implements Comparator<Contact> {
+    @Override
+    public int compare(Contact o1, Contact o2) {
+        return o1.getName().compareTo(o2.getName());
+    }
 }
