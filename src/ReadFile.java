e38e7884bd64501d4763949be66f7857459535ef
tuuyen09 <tu190901@gmail.com>
2023-04-16 12:28:48 +0700

    ReadFile

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class ReadFile {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadFile {
+    public static List<Contact> readFile(){
+        List<Contact> contactList = new ArrayList<>();
+        try {
+            File file = new File("contact.txt");
+            InputStream is = new FileInputStream(file);
+            ObjectInputStream ois = new ObjectInputStream(is);
+            contactList = (List<Contact>) ois.readObject();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return contactList;
+    }
 }
